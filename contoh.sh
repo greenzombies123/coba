db26f8e22b910bdf22c1b2a66f5090013bb9d349
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-24 16:43:42 +0700

    Add files via upload

---
+++ contoh.sh
@@ -0,0 +1,89 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo 
+echo
+echo
+echo $red" @@@@@@@@  @@@@@@@   @@@@@@@@  @@@@@@@@  @@@  @@@  "
+sleep 1
+echo $red"@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@ @@@  "
+sleep 1
+echo $red"!@@        @@!  @@@  @@!       @@!       @@!@!@@@  "
+sleep 1
+echo $red"!@!        !@!  @!@  !@!       !@!       !@!!@!@!  "
+sleep 1
+echo $white"!@! @!@!@  @!@!!@!   @!!!:!    @!!!:!    @!@ !!@!  "
+sleep 1
+echo $white"!!! !!@!!  !!@!@!    !!!!!:    !!!!!:    !@!  !!!  "
+sleep 1
+echo $white":!!   !!:  !!: :!!   !!:       !!:       !!:  !!!  "
+sleep 1
+echo $white":!:   !::  :!:  !:!  :!:       :!:       :!:  !:!  "
+echo $white" ::: ::::  ::   :::   :: ::::   :: ::::   ::   ::  "
+echo $white" :: :: :    :   : :  : :: ::   : :: ::   ::    :   "
+echo
+echo $red"                                                                    "
+echo $red"@@@@@@@@   @@@@@@   @@@@@@@@@@   @@@@@@@   @@@  @@@@@@@@   @@@@@@   "
+echo $red"@@@@@@@@  @@@@@@@@  @@@@@@@@@@@  @@@@@@@@  @@@  @@@@@@@@  @@@@@@@   "
+sleep 1
+echo $red"     @@!  @@!  @@@  @@! @@! @@!  @@!  @@@  @@!  @@!       !@@       "
+echo $red"    !@!   !@!  @!@  !@! !@! !@!  !@   @!@  !@!  !@!       !@!       "
+echo $red"   @!!    @!@  !@!  @!! !!@ @!@  @!@!@!@   !!@  @!!!:!    !!@@!!    "
+echo $white"  !!!     !@!  !!!  !@!   ! !@!  !!!@!!!!  !!!  !!!!!:     !!@!!!   "
+echo $white" !!:      !!:  !!!  !!:     !!:  !!:  !!!  !!:  !!:            !:!  "
+echo $white":!:       :!:  !:!  :!:     :!:  :!:  !:!  :!:  :!:           !:!   "
+echo $white" :: ::::  ::::: ::  :::     ::    :: ::::   ::   :: ::::  :::: ::   "
+echo $white": :: : :   : :  :    :      :    :: : ::   :    : :: ::   :: : :    "
+echo
+figlet -f smshadow "zombies"
+figlet -f smshadow "installer"
+echo $indigo
+date
+echo $red"___________________________________________________"
+echo $red" [+]Author by    : GREEN ZOMBIES "
+echo $red" [+]Team           : AGEN SEA         "
+echo $red" [+]Thanks to      : MR_81"
+sleep 1
+echo $red" [+]Thanks to      : MR_ARIANIMOUS"
+echo $white" [+]Thanks to      : MR_XZ.6"
+sleep 1
+echo $white" [+]Thanks to      : B3kT0509"
+echo $white" [+]Thanks to      : MR_VIRUS"
+echo $white" [+]Thanks to      : MR_DENIMNEWBIE"
+echo $white" [+]Thanks to      :꧁࿗ｲu4刀乙•ｱ尺o√oズﾑｲo尺࿗꧂"
+echo $white"___________________________________________________"
+echo
+echo
+echo
+echo
+echo
+echo
+echo $red" @@@@@@    @@@@@@@@  @@@@@@@@  @@@  @@@"
+echo $white"@@@@@@@@  @@@@@@@@@  @@@@@@@@  @@@@ @@@"
+echo $red"@@!  @@@  !@@        @@!       @@!@!@@@"
+sleep 1
+echo $white"!@!  @!@  !@!        !@!       !@!!@!@!"
+echo $red"@!@!@!@!  !@! @!@!@  @!!!:!    @!@ !!@!"
+echo $white"!!!@!!!!  !!! !!@!!  !!!!!:    !@!  !!!"
+echo $red"!!:  !!!  :!!   !!:  !!:       !!:  !!!"
+echo $white":!:  !:!  :!:   !::  :!:       :!:  !:!"
+echo $red"::   :::   ::: ::::   :: ::::   ::   ::"
+echo $white" :   : :   :: :: :   : :: ::   ::    :"
+echo
+echo $red" @@@@@@   @@@@@@@@   @@@@@@"
+echo $white"@@@@@@@   @@@@@@@@  @@@@@@@@"
+echo $red"!@@       @@!       @@!  @@@"
+sleep 2
+echo $white"!@!       !@!       !@!  @!@"
+echo $red"!!@@!!    @!!!:!    @!@!@!@!"
+echo $white" !!@!!!   !!!!!:    !!!@!!!!"
+echo $red"     !:!  !!:       !!:  !!!"
+echo $white"    !:!   :!:       :!:  !:!"
+echo $red":::: ::    :: ::::  ::   :::"
+echo $white":: : :    : :: ::    :   : :"
+echo
